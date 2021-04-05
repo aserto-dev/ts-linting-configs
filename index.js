c61a166ed3edcd3b1f7e6a9633a511018b401093
Eduardo <drdpedroso@gmail.com>
2021-04-05 15:34:21 -0300

    Change rules for eslint

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ module.exports = {
         'prettier/prettier': ['error'],
         'react/jsx-one-expression-per-line': 'off',
         'no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'react/display-name': 'off',
     },
     ignorePatterns: ['dist/**/*.js'],
     parserOptions: {
