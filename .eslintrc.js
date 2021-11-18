5235dc9dbc50de38df2fbd83223e1c5247762fc3
authereal <authereal@users.noreply.github.com>
2021-11-17 20:06:32 -0500

    Consolidate prettier rules and remove superfluous comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
   extends: [
     'prettier',
-    'prettier/react',
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:react-hooks/recommended', // Uses the recommended rules from react hooks
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // This will display prettier errors as ESLint errors.
+    // Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended',
   ],
   rules: {
-    eqeqeq: 'always',
+    eqeqeq: ['error', 'always'],
     'no-use-before-define': 'off',
     '@typescript-eslint/consistent-type-assertions': [
       'error',
